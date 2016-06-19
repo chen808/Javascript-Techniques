5da5dd5eed8e8a9cafa26ee3b6066e990440d8c3
David Chen <chentiki808@gmail.com>
2016-06-18 23:17:44 -0700

    finished simple weather app

--- JS_Weather/scripts.js
+++ JS_Weather/scripts.js
@@ -1,49 +1,38 @@
-
 $(document).ready(function(){
 
 
    var getPoster = function(){
 
-        var film = $('#term').val();
+        // getting the zipcode from html
+        var zip = $('#zipcode').val();
 
-        console.log(film);
+        // test print
+        console.log(zip);
 
-         if(film == ''){
+        // get api key from account, no # after 'APPID' for security reason
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + zip + "&APPID=", function(json) {
 
-            $('#poster').html("<h2 class='loading'>Ha! We haven't forgotten to validate the form! Please enter something.</h2>");
+          console.log(json);
+          // getting city name
+          console.log(json.name);
+          // getting description
+          console.log(json.weather[0].description);
+          // getting humidity
+          console.log(json.main.humidity);
+          // getting latitude
+          console.log(json.coord.lat);
+          // getting longitude
+          console.log(json.coord.lon);
 
-         } else {
-
-            $('#poster').html("<h2 class='loading'>Your poster is on its way!</h2>");
-
-            // get api key from account, no # after 'APPID' for security reason
-            $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=95129&APPID=", function(json) {
-
-              console.log(json);
-              // getting city name
-              console.log(json.name);
-              // getting description
-              console.log(json.weather[0].description);
-
-              console.log(json.main.humidity);
-
-              console.log(json.coord.lat);
-
-              console.log(json.coord.lon);
+          $('#showWeather').html("<h2>The current weather in " + json.name + " is: " + json.weather[0].description + "</h2>");
 
 
-             });
+        });
+      };
 
-          }
 
-        return false;
-   }
-
+   // when search button is clicked, invoke 'getPoster' function
    $('#search').click(getPoster);
-   $('#term').keyup(function(event){
-       if(event.keyCode == 13){
-           getPoster();
-       }
-   });
+
 
 });
